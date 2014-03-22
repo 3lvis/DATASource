ce1ce4310d5c5895f6bfa5635f9b78847bff2ba8
Elvis Nunez <elvisnunez@me.com>
2014-03-22 08:04:14 +0100

    Use ANDYDatabaseManager from Cocoapods.

---
+++ Demo/Pods/BuildHeaders/ANDYDatabaseManager/ANDYDatabaseManager.h
@@ -0,0 +1,1 @@
+../../ANDYDatabaseManager/ANDYDatabaseManager/ANDYDatabaseManager.h
---
+++ Demo/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ANDYDatabaseManager
+#define COCOAPODS_POD_AVAILABLE_ANDYDatabaseManager
+#define COCOAPODS_VERSION_MAJOR_ANDYDatabaseManager 0
+#define COCOAPODS_VERSION_MINOR_ANDYDatabaseManager 2
+#define COCOAPODS_VERSION_PATCH_ANDYDatabaseManager 0
+
