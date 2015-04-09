3ec62e05c59adf843c3612e4c9d3947505df73fc
Elvis Nuñez <elvisnunez@me.com>
2015-04-09 02:03:08 +0200

    Add base example

---
+++ Demos/TableView/TableView/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+import CoreData
+import DATAStack
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+  lazy var dataStack = DATAStack()
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+
+    return true
+  }
+}
+
