c108b8cd6246e60bc2c3e21e4c3dee32ffc3668e
Elvis Nuñez <elvisnunez@me.com>
2015-04-14 23:07:42 +0200

    Improve example

--- Demos/TableView/TableView/ViewController.swift
+++ Demos/TableView/TableView/ViewController.swift
@@ -24,10 +24,11 @@ class ViewController: UITableViewController {
 
     self.tableView.dataSource = DATASource(
         tableView: self.tableView,
-        fetchRequest: request, cellIdentifier: "Cell",
+        fetchRequest: request,
+        cellIdentifier: "Cell",
         mainContext: self.dataStack.mainContext,
         configuration: { (cell, item, indexPath) -> Void in
-            let cell = cell as UITableViewCell
+            let cell = cell as! UITableViewCell
             cell.textLabel!.text = "Hi"
     })
   }
