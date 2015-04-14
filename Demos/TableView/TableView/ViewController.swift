fe0e62e536ff7afe2531943bf49dccb7a5c8fcb6
Christoffer Winterkvist <chris@zenangst.com>
2015-04-15 00:43:14 +0200

    Make dataStack into a constant

--- Demos/TableView/TableView/ViewController.swift
+++ Demos/TableView/TableView/ViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import DATAStack
 
 class ViewController: UITableViewController {
-  var dataStack: DATAStack
+  let dataStack: DATAStack
 
   required init(coder aDecoder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
