9168980101a97210a8618fd83260b8182b9e4329
Elvis Nuñez <elvisnunez@me.com>
2015-04-15 16:32:58 +0200

    Clean up

--- Demos/TableView/TableSwift/ViewController.swift
+++ Demos/TableView/TableSwift/ViewController.swift
@@ -1,6 +1,15 @@
 import UIKit
+import DATAStack
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    var dataStack: DATAStack?
+
+    convenience init(dataStack: DATAStack) {
+        self.init(style: .Plain)
+
+        self.dataStack = dataStack
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
