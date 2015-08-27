ac53aa33d1576acd8e6af41e84e73d3d749fa53c
Elvis Nuñez <elvisnunez@me.com>
2015-08-27 13:31:47 +0200

    Update TableView Demo and Swift demo

--- Demos/TableView/TableSwift/ViewController.swift
+++ Demos/TableView/TableSwift/ViewController.swift
@@ -15,14 +15,15 @@ class ViewController: UITableViewController {
         request.sortDescriptors = [NSSortDescriptor(key: "name",
             ascending: true)]
 
-        self.dataSource = DATASource(
-            tableView: self.tableView,
+        self.dataSource = DATASource(tableView: self.tableView,
             fetchRequest: request,
+            sectionName: "firstLetterOfName",
             cellIdentifier: "Cell",
             mainContext: self.dataStack!.mainContext,
             configuration: { (cell, item, indexPath) -> Void in
                 let cell = cell as! UITableViewCell
-                cell.textLabel!.text = "Hi"
+                let item = item as! NSManagedObject
+                cell.textLabel!.text = item.valueForKey("name") as? String
         })
     }
 
@@ -31,7 +32,7 @@ class ViewController: UITableViewController {
 
         self.tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "Cell")
 
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Save, target: self, action: "saveAction")
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "saveAction")
 
         self.tableView.dataSource = self.dataSource
     }
@@ -40,7 +41,12 @@ class ViewController: UITableViewController {
         self.dataStack!.performInNewBackgroundContext { (backgroundContext) -> Void in
             if let entity = NSEntityDescription.entityForName("User", inManagedObjectContext: backgroundContext) {
                 let user = NSManagedObject(entity: entity, insertIntoManagedObjectContext: backgroundContext)
-                user.setValue("Elvis", forKey: "name")
+
+                let name = self.randomString()
+                let firstLetter = String(Array(name)[0])
+                user.setValue(name, forKey: "name")
+                user.setValue(firstLetter.uppercaseString, forKey: "firstLetterOfName")
+
                 var error: NSError?
                 if !backgroundContext.save(&error) {
                     println("Could not save \(error), \(error?.userInfo)")
@@ -49,6 +55,19 @@ class ViewController: UITableViewController {
                 println("Oh no")
             }
         }
+    }
+
+    func randomString() -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        var string = ""
+        for index in 0...10 {
+            let token = UInt32(count(letters))
+            let letterIndex = Int(arc4random_uniform(token))
+            var firstChar = Array(letters)[letterIndex]
+            string.append(firstChar)
+        }
+
+        return string
     }
 }
 
