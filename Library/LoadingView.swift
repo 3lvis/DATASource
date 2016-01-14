50d1f6d862483ec515d71501423531249630a089
Elvis Nuñez <elvisnunez@me.com>
2016-01-14 15:59:16 +0100

    Add fun bouncing loading indicator

--- Library/LoadingView.swift
+++ Library/LoadingView.swift
@@ -2,42 +2,34 @@ import UIKit
 
 class LoadingView: UIView {
     lazy var loadingIndicator: UIActivityIndicatorView = {
-        let loadingIndicator = UIActivityIndicatorView(activityIndicatorStyle: .White)
+        let loadingIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+        loadingIndicator.startAnimating()
 
         return loadingIndicator
     }()
 
-    lazy var loadingLabel: UILabel = {
-        let loadingLabel = UILabel()
-        loadingLabel.textAlignment = .Center
-        loadingLabel.textColor = UIColor.whiteColor()
-        loadingLabel.font = UIFont.systemFontOfSize(14)
-
-        return loadingLabel
-    }()
-
     init() {
         let bounds = UIScreen.mainScreen().bounds
-        let width = CGFloat(150)
-        let height = CGFloat(35)
+        let width = CGFloat(40)
+        let height = CGFloat(40)
         let bottomMargin = CGFloat(25)
         let x = (bounds.width - width) / 2
         let y = bounds.height - height - bottomMargin
         let frame = CGRect(x: x, y: y, width: width, height: height)
         super.init(frame: frame)
 
-        self.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.6)
+        self.backgroundColor = UIColor.whiteColor()
         self.layer.cornerRadius = height / 2
+        self.layer.shadowColor = UIColor.blackColor().CGColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.shadowRadius = 2
+        self.layer.shadowOpacity = 0.3
 
-        let rightMargin = CGFloat(15)
         var loadingIndicatorFrame = self.loadingIndicator.frame
-        loadingIndicatorFrame.origin.x = width - loadingIndicatorFrame.width - rightMargin
+        loadingIndicatorFrame.origin.x = (width - loadingIndicatorFrame.width) / 2
         loadingIndicatorFrame.origin.y = (height - loadingIndicatorFrame.height) / 2
         self.loadingIndicator.frame = loadingIndicatorFrame
         self.addSubview(self.loadingIndicator)
-
-        self.loadingLabel.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
-        self.addSubview(self.loadingLabel)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -45,23 +37,31 @@ class LoadingView: UIView {
     }
 
     func present() {
-        self.loadingIndicator.startAnimating()
-        self.loadingLabel.text = "Loading"
-
+        let originalFrame = self.frame
+        self.frame.origin.y = UIScreen.mainScreen().bounds.height
         if self.superview == nil {
             let window = UIApplication.sharedApplication().keyWindow!
             window.addSubview(self)
+
+            UIView.animateWithDuration(0.3, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [.AllowUserInteraction], animations: {
+                self.frame = originalFrame
+                }, completion: nil)
         }
     }
 
     func dismiss() {
-        self.loadingIndicator.stopAnimating()
-        self.loadingLabel.text = "Done"
-
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.3 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
-            if self.superview != nil {
-                self.removeFromSuperview()
-            }
+            let originalFrame = self.frame
+            var newFrame = self.frame
+            newFrame.origin.y = UIScreen.mainScreen().bounds.height
+            UIView.animateWithDuration(0.3, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [.AllowUserInteraction], animations: { () -> Void in
+                self.frame = newFrame
+                }, completion: { finished in
+                    self.frame = originalFrame
+                    if self.superview != nil {
+                        self.removeFromSuperview()
+                    }
+            })
         }
     }
 }
