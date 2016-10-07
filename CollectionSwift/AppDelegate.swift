3811afa6c11d4e1517fadec53936c2e25e715e8d
Elvis Nuñez <elvisnunez@me.com>
2016-10-07 22:24:03 +0200

    Fix broken CollectionSwift demo

--- CollectionSwift/AppDelegate.swift
+++ CollectionSwift/AppDelegate.swift
@@ -3,33 +3,31 @@ import CoreData
 import DATAStack
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow? = {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-
-        return window
-        }()
+class AppDelegate: UIResponder {
+    var window: UIWindow?
 
     var dataStack: DATAStack = {
         let dataStack = DATAStack(modelName: "DataModel")
 
         return dataStack
     }()
+}
 
-    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: Any]?) -> Bool {
-        if let window = self.window {
-            let bounds = UIScreen.main.bounds
+extension AppDelegate: UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = self.window else { fatalError("Window not found") }
 
-            let layout = UICollectionViewFlowLayout()
-            layout.itemSize = CGSize(width: 120, height: 120)
-            layout.sectionInset = UIEdgeInsets(top: 15, left: 0, bottom: 15, right: 0)
-            layout.headerReferenceSize = CGSize(width: bounds.size.width, height: 60)
+        let bounds = UIScreen.main.bounds
 
-            let viewController = CollectionController(layout: layout, dataStack: self.dataStack)
-            window.rootViewController = UINavigationController(rootViewController: viewController)
-            window.makeKeyAndVisible()
-        }
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 120, height: 120)
+        layout.sectionInset = UIEdgeInsets(top: 15, left: 0, bottom: 15, right: 0)
+        layout.headerReferenceSize = CGSize(width: bounds.size.width, height: 60)
+
+        let viewController = CollectionController(layout: layout, dataStack: self.dataStack)
+        window.rootViewController = UINavigationController(rootViewController: viewController)
+        window.makeKeyAndVisible()
 
         return true
     }
